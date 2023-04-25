2c94c75b548ac84b3a35765a1efe680e2f45b6a6
Fernando Palafox <fernando.palafox@colorado.edu>
2023-04-25 17:26:41 -0500

    first commit

---
+++ experiments/rotating_hyperplane.jl
@@ -0,0 +1,95 @@
+# Imports
+import Ipopt
+import TestDynamics
+
+using PartiallyObservedInverseGames.ForwardGame: IBRGameSolver, solve_game, KKTGameSolver
+using JuMP: @objective
+
+using VegaLite: VegaLite
+using PartiallyObservedInverseGames.TrajectoryVisualization:
+    TrajectoryVisualization, visualize_trajectory
+using CollisionAvoidanceGame: CollisionAvoidanceGame
+
+unique!(push!(LOAD_PATH, joinpath(@__DIR__, "utils")))
+include("./utils/misc.jl")
+
+# ---- Setup ---- 
+
+T = 25
+
+control_system =
+    TestDynamics.ProductSystem([TestDynamics.Unicycle(0.25), TestDynamics.Unicycle(0.25)])
+
+player_angles = let
+    n_players = length(control_system.subsystems)
+    map(eachindex(control_system.subsystems)) do ii
+        angle_fraction = n_players == 2 ? pi / 2 : 2pi / n_players
+        (ii - 1) * angle_fraction
+    end
+end
+
+x0 = mapreduce(vcat, player_angles) do player_angle
+    [unitvector(player_angle + pi); 0.1; player_angle + deg2rad(10)]
+end
+
+player_cost_models = map(enumerate(player_angles)) do (ii, player_angle)
+    cost_model_p1 = CollisionAvoidanceGame.generate_player_cost_model(;
+        player_idx = ii,
+        control_system,
+        T,
+        goal_position = unitvector(player_angle),
+    )
+end
+
+
+# ---- Solve FG ---- 
+ibr_converged, ibr_solution, ibr_models =
+        solve_game(IBRGameSolver(), control_system, player_cost_models, x0, T)
+
+# ---- FG with Solve KKT ----
+kkt_converged, kkt_solution, kkt_model = solve_game(
+        KKTGameSolver(),
+        control_system,
+        player_cost_models,
+        x0,
+        T;
+        solver = Ipopt.Optimizer,
+    )
+visualize_trajectory(control_system, kkt_solution.x, canvas = VegaLite.@vlplot(width = 400, height = 400))
+
+# ---- Visualize ----
+
+trajectory_data_gt =
+    TrajectoryVisualization.trajectory_data(control_system, ibr_solution.x)
+
+trajectory_viz_config = (;
+    x_position_domain = (-1.2, 1.2),
+    y_position_domain = (-1.2, 1.2),
+    opacity = 0.5,
+    legend = false,
+)
+
+groups = [
+    "Ground Truth"
+]
+color_map = Dict([
+    "Ground Truth" => "black"
+])
+color_scale =
+    VegaLite.@vlfrag(domain = groups, range = [color_map[g] for g in groups])
+
+ground_truth_viz =
+    TrajectoryVisualization.visualize_trajectory(
+        trajectory_data_gt;
+        canvas = VegaLite.@vlplot(width = 400, height = 400),
+        legend = VegaLite.@vlfrag(orient = "top", offset = 5),
+        trajectory_viz_config...,
+        group = "Ground Truth",
+        color_scale,
+    ) + VegaLite.@vlplot(
+        data = filter(s -> s.t == 1, trajectory_data_gt),
+        mark = {"text", dx = 8, dy = 8},
+        text = "player",
+        x = "px:q",
+        y = "py:q",
+    )
