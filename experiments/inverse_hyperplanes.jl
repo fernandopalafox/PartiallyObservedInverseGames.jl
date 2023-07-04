bae817f1b9d8aee7e897f88713626539532bdedf
Fernando Palafox <fernando.palafox@colorado.edu>
2023-07-03 18:01:23 -0600

    working 3p inv. game. commented out x and u as decision vars

--- experiments/inverse_hyperplanes.jl
+++ experiments/inverse_hyperplanes.jl
@@ -34,27 +34,25 @@ end
 
 # ---- USER INPUT: Solver settings ----
 μ = 0.00001
-n_players = 2
 ΔT = 0.25
-solver_attributes = (; print_level = 5, expect_infeasible_problem = "yes")
+solver_attributes = (; print_level = 5, expect_infeasible_problem = "no")
 
 # ---- Solve ---- 
 
 # Presumed system dynamics
-# control_system =
-#     TestDynamics.ProductSystem([TestDynamics.HyperUnicycle(ΔT, 0.0, 0.0), 
-#                                 TestDynamics.HyperUnicycle(ΔT, 0.0, 0.0),
-#                                 TestDynamics.HyperUnicycle(ΔT, 0.0, 0.0)])
 control_system =
     TestDynamics.ProductSystem([TestDynamics.HyperUnicycle(ΔT, 0.0, 0.0), 
-                                TestDynamics.HyperUnicycle(ΔT, 0.0, 0.0)])
+                                TestDynamics.HyperUnicycle(ΔT, 0.0, 0.0),
+                                TestDynamics.Unicycle(ΔT)])
+
 @unpack n_states, n_controls = control_system
+n_players = length(control_system.subsystems)
 n_states_per_player = Int(n_states / n_players)
 
 # Setup solver
 opt_model = JuMP.Model(Ipopt.Optimizer)
 set_solver_attributes!(opt_model; solver_attributes...)
-JuMP.set_time_limit_sec(opt_model, 120.0)
+JuMP.set_time_limit_sec(opt_model, 80.0)
 
 # Load observation data
 data_states   = Matrix(CSV.read("data/KKT_trajectory_state.csv", DataFrame, header = false))
@@ -76,58 +74,53 @@ player_cost_models = map(enumerate(player_angles)) do (ii, player_angle)
         weights = (; control_Δv = 10, control_Δθ = 1, state_goal = 1)
     )
 end
-player_cost_models = map(enumerate(player_angles)) do (ii, player_angle)
-    cost_model_p1 = CollisionAvoidanceGame.generate_player_cost_model_simple(;
-        player_idx = ii,
-        control_system,
-        T,
-        goal_position = unitvector(player_angle),
-    )
-end
 
 # ---- USER INPUT: Setup unknown parameters ----
-# uk_ωs = @variable(opt_model, [1:3])
-# uk_αs = @variable(opt_model, [1:3])
-# uk_ρs = @variable(opt_model, [1:3])
+uk_ωs = @variable(opt_model, [1:3], lower_bound = -pi/3, upper_bound = pi/3)
+uk_αs = @variable(opt_model, [1:3], lower_bound = -pi, upper_bound = pi)
+JuMP.set_start_value(uk_ωs[3], -0.05) 
+# uk_ρs = @variable(opt_model, [1:3], start = 0.25)
 
-# ωs = [0.0 uk_ωs[1] uk_ωs[2];
-#       0.0 0.0      uk_ωs[3];
-#       0.0 0.0      0.0]
-# αs = [0.0 uk_αs[1] uk_αs[2];
-#       0.0 0.0      uk_αs[3];
-#       0.0 0.0      0.0]
-# αs = [0.0 uk_ρs[1] uk_ρs[2];
-#       0.0 0.0      uk_ρs[3];
-#       0.0 0.0      0.0]
-# ω  = @variable(opt_model)
-# ωs = [0.0 0.03 0.03;
-#       0.0 0.0    ω;
-#       0.0 0.0  0.0]
-# ρs = [0.0 0.25 0.25;
-#       0.0 0.0 0.1;
-#       0.0 0.0 0.0]
+ωs = [0.0 uk_ωs[1] uk_ωs[2];
+      0.0 0.0      uk_ωs[3];
+      0.0 0.0      0.0]
+ρs = [0.0 0.25     0.25;
+      0.0 0.0      0.1;
+      0.0 0.0      0.0]
+αs = [0.0 uk_αs[1] uk_αs[2];
+      0.0 0.0      uk_αs[3];
+      0.0 0.0      0.0]
+
+# ω2 = @variable(opt_model, start = 0.05)
+# ω3 = @variable(opt_model, start = -0.05)
+# α  = @variable(opt_model, lower_bound = -pi, upper_bound = pi)
+# ωs = [0.0 0.03   ω2;
+#       0.0 0.0    ω3;
+#       0.0 0.0    0.0]
+# ρs = [0.0 0.25   0.25;
+#       0.0 0.0    0.1;
+#       0.0 0.0    0.0]
 # αs = [0.0 3/4*pi pi;
-#       0.0 0.0 5/4*pi;
-#       0.0 0.0 0.0]
+#       0.0 0.0    α;
+#       0.0 0.0    0.0]
 
-# adj_mat = [false true  true; 
-#            false false true;
-#            false false false]
+adj_mat = [false true  true; 
+           false false true;
+           false false false]
 
-ω  = @variable(opt_model)
-α  = @variable(opt_model)
-ωs = [0.0 ω] 
-ρs = [0.0 0.25]
-αs = [0.0 α] 
-adj_mat = [false true; 
-           false false]
+# ω  = @variable(opt_model)
+# α  = @variable(opt_model)
+# ωs = [0.0 ω] 
+# ρs = [0.0 0.25]
+# αs = [0.0 α] 
+# adj_mat = [false true; 
+#            false false]
 
-constraint_params = (; adj_mat, ωs, αs, ρs)
-
-init = (;x = data_states, u = data_controls)
 init = (;x = data_states, u = data_controls, 
          λ_e = kkt_solution.λ_e, λ_i_all = kkt_solution.λ_i_all, 
          s_all = kkt_solution.s_all)
+
+constraint_params = (; adj_mat, ωs, αs, ρs)
 
 # ---- Setup decision variables ----
 
@@ -137,25 +130,26 @@ if !isnothing(constraint_params.adj_mat)
 end
 
 # Other decision variables
-x       = @variable(opt_model, [1:n_states, 1:T])
-u       = @variable(opt_model, [1:n_controls, 1:T])
+# x       = @variable(opt_model, [1:n_states, 1:T])
+x       = data_states
+u       = data_controls
+# u       = @variable(opt_model, [1:n_controls, 1:T])
 λ_e     = @variable(opt_model, [1:n_states, 1:(T - 1), 1:n_players])
 λ_i_all = @variable(opt_model, [1:length(couples), 1:T]) # Assumes constraints apply to all timesteps. All constraints the same
 s_all   = @variable(opt_model, [1:length(couples), 1:T], start = 0.001, lower_bound = 0.0)     
 
 # Warms start on decision variables
-init_if_hasproperty!(x, init, :x)
-init_if_hasproperty!(u, init, :u)
+# init_if_hasproperty!(x, init, :x)
+# init_if_hasproperty!(u, init, :u)
 init_if_hasproperty!(λ_e, init, :λ_e)
 init_if_hasproperty!(λ_i_all, init, :λ_i_all)
 init_if_hasproperty!(s_all, init, :s_all)
 
 
 # Constraint on initial position
-@constraint(opt_model, x[:, 1] .== x0)
+# @constraint(opt_model, x[:, 1] .== x0)
 
 # constraints
-@constraint(opt_model, x[:, 1] .== x0)
 DynamicsModelInterface.add_dynamics_constraints!(control_system, opt_model, x, u)
 df = DynamicsModelInterface.add_dynamics_jacobians!(control_system, opt_model, x, u)
 
@@ -181,13 +175,13 @@ for (player_idx, cost_model) in enumerate(player_cost_models)
                                 )
 
             # Extract shared constraint Jacobian 
-            dhs = DynamicsModelInterface.add_shared_jacobian!(
-                                    control_system.subsystems[player_idx], opt_model, x, u, params
-                                )
+            # dhs = DynamicsModelInterface.add_shared_jacobian!(
+            #                         control_system.subsystems[player_idx], opt_model, x, u, params
+            #                     )
 
             # Stack shared constraint Jacobian. 
             # One row per couple, timestep indexing along 3rd axis
-            append!(dhdx_container, [dhs.dx]) 
+            # append!(dhdx_container, [dhs.dx]) 
 
             # Feasibility of barrier-ed constraints
             @constraint(opt_model, [t = 1:T], hs(t) - s[couple_idx, t] == 0.0)
@@ -195,19 +189,19 @@ for (player_idx, cost_model) in enumerate(player_cost_models)
         dhdx = vcat(dhdx_container...)
     
         # Gradient of the Lagrangian wrt x is zero 
-        @constraint(opt_model, 
-        [t = 2:(T-1)],
-            dJ.dx[:, t]' + λ_e[:, t - 1, player_idx]' - λ_e[:, t, player_idx]'*df.dx[:, :, t] + λ_i[:,t]'*dhdx[:, :, t] .== 0
-        )
-        @constraint(opt_model, 
-            dJ.dx[:, T]' + λ_e[:, T - 1, player_idx]' + λ_i[:,T]'*dhdx[:, :, T] .== 0
-        )   
+        # @constraint(opt_model, 
+        # [t = 2:(T-1)],
+        #     dJ.dx[:, t]' + λ_e[:, t - 1, player_idx]' - λ_e[:, t, player_idx]'*df.dx[:, :, t] + λ_i[:,t]'*dhdx[:, :, t] .== 0
+        # )
+        # @constraint(opt_model, 
+        #     dJ.dx[:, T]' + λ_e[:, T - 1, player_idx]' + λ_i[:,T]'*dhdx[:, :, T] .== 0
+        # )   
 
         # Gradient of the Lagrangian wrt player's own inputs is zero
-        @constraint(opt_model, 
-        [t = 1:(T-1)], 
-            dJ.du[player_inputs, t]' - λ_e[:, t, player_idx]'*df.du[:,player_inputs,t] .== 0)
-        @constraint(opt_model, dJ.du[player_inputs, T]' .== 0)
+        # @constraint(opt_model, 
+        # [t = 1:(T-1)], 
+        #     dJ.du[player_inputs, t]' - λ_e[:, t, player_idx]'*df.du[:,player_inputs,t] .== 0)
+        # @constraint(opt_model, dJ.du[player_inputs, T]' .== 0)
 
         # Gradient of the Lagrangian wrt s is zero
         n_slacks     = length(s)
@@ -219,53 +213,51 @@ for (player_idx, cost_model) in enumerate(player_cost_models)
         
     else
         # KKT Nash constraints
-        @constraint(
-            opt_model,
-            [t = 2:(T - 1)],
-            dJ.dx[:, t] + λ_e[:, t - 1, player_idx] - (λ_e[:, t, player_idx]' * df.dx[:, :, t])' .== 0
-        )
-        @constraint(opt_model, dJ.dx[:, T] + λ_e[:, T - 1, player_idx] .== 0)
+        # @constraint(
+        #     opt_model,
+        #     [t = 2:(T - 1)],
+        #     dJ.dx[:, t] + λ_e[:, t - 1, player_idx] - (λ_e[:, t, player_idx]' * df.dx[:, :, t])' .== 0
+        # )
+        # @constraint(opt_model, dJ.dx[:, T] + λ_e[:, T - 1, player_idx] .== 0)
 
-        @constraint(
-            opt_model,
-            [t = 1:(T - 1)],
-            dJ.du[player_inputs, t] - (λ_e[:, t, player_idx]' * df.du[:, player_inputs, t])' .== 0
-        )
-        @constraint(opt_model, dJ.du[player_inputs, T] .== 0)            
-        # println("Added KKT conditions for player $player_idx")
+        # @constraint(
+        #     opt_model,
+        #     [t = 1:(T - 1)],
+        #     dJ.du[player_inputs, t] - (λ_e[:, t, player_idx]' * df.du[:, player_inputs, t])' .== 0
+        # )
+        # @constraint(opt_model, dJ.du[player_inputs, T] .== 0)            
+        # # println("Added KKT conditions for player $player_idx")
     end
  
 end
 
 # Match equilibirum
-@objective(opt_model, Min, sum(el -> el^2, data_states .- x))
+# @objective(opt_model, Min, sum(el -> el^2, data_states .- x))
 
 time = @elapsed JuMP.optimize!(opt_model)
 @info time
 
-# solution = get_values(;x, u, uk_ωs, uk_αs, uk_ρs)
-# k_ωs = [0.0 solution.uk_ωs[1] solution.uk_ωs[2];
-#         0.0 0.0               solution.uk_ωs[3];
-#         0.0 0.0               0.0]
-# k_αs = [0.0 solution.uk_αs[1] solution.uk_αs[2];
-#         0.0 0.0               solution.uk_αs[3];
-#         0.0 0.0               0.0]
-# k_ρs = [0.0 solution.uk_ρs[1] solution.uk_ρs[2];
-#         0.0 0.0               solution.uk_ρs[3];
-#         0.0 0.0               0.0]
-solution = get_values(;x, u, ω, α)
-# k_ωs = [0.0 0.03 0.03;
-#       0.0 0.0  solution.ω;
-#       0.0 0.0  0.0]
+solution = get_values(;x, u, uk_ωs, uk_αs)
+k_ωs = [0.0 solution.uk_ωs[1] solution.uk_ωs[2];
+        0.0 0.0               solution.uk_ωs[3];
+        0.0 0.0               0.0]
+k_αs = [0.0 solution.uk_αs[1] solution.uk_αs[2];
+        0.0 0.0               solution.uk_αs[3];
+        0.0 0.0               0.0]
+k_ρs = ρs
+# solution = get_values(;x, u, ω2, ω3, α)
+# k_ωs = [0.0 0.03 solution.ω2;
+#         0.0 0.0  solution.ω3;
+#         0.0 0.0  0.0]
 # k_ρs = [0.0 0.25 0.25;
-#       0.0 0.0 0.1;
-#       0.0 0.0 0.0]
+#         0.0 0.0   0.1;
+#         0.0 0.0   0.0]
 # k_αs = [0.0 3/4*pi pi;
-#       0.0 0.0 5/4*pi;
-#       0.0 0.0 0.0]
-k_ωs = [0.0 solution.ω] 
-k_ρs = [0.0 0.25]
-k_αs = [0.0 solution.α] 
+#         0.0 0.0    solution.α;
+#         0.0 0.0    0.0]
+# k_ωs = [0.0 solution.ω] 
+# k_ρs = [0.0 0.25]
+# k_αs = [0.0 solution.α] 
 
-# visualize_rotating_hyperplanes(solution.x,(; ωs = k_ωs, αs = k_αs, ρs = k_ρs, title = "Inverse"))
-visualize_rotating_hyperplane(solution.x,(; ωs = k_ωs, ρs = k_ρs, αs = k_αs, title = "Inverse"))
+visualize_rotating_hyperplanes(solution.x,(; ωs = k_ωs, αs = k_αs, ρs = k_ρs, title = "Inverse"))
+# visualize_rotating_hyperplane(solution.x,(; ωs = k_ωs, ρs = k_ρs, αs = k_αs, title = "Inverse"))
