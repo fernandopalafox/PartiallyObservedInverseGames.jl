d6f21c4081126661d1de5a395f9688f8924c3a04
fernandopalafox <fernandopalafox@utexas.edu>
2023-07-06 12:18:38 -0500

    fixed initial orientations and nice example

--- experiments/generate_integrators.jl
+++ experiments/generate_integrators.jl
@@ -18,13 +18,15 @@ using CSV, DataFrames
 include("utils/misc.jl")
 
 # ---- Setup ---- 
-T = 75
-T_activate_goalcost = Int(round(T/3))
-ΔT = 0.25
+T = 50
+T_activate_goalcost = 1
+ΔT = 0.1
 n_players = 4
-v_init = 0.3
-offset = 90
+v_init = 3
+os = deg2rad(90) # init. angle offset
 scale = 10.0
+
+max_wall_time = 60.0
 
 # Setup system
 control_system = TestDynamics.ProductSystem([TestDynamics.DoubleIntegrator(ΔT) for _ in 1:n_players])
@@ -33,7 +35,7 @@ as = [a > pi ? a - 2*pi : a for a in as]
 
 x0 = vcat(
     [
-        vcat(-scale*unitvector(a), [v_init, a - deg2rad(offset)]) for
+        vcat(-scale*unitvector(a), [v_init*cos(a - os), v_init*sin(a - os)]) for
         a in as
     ]...,
 )
@@ -48,8 +50,8 @@ player_cost_models = map(enumerate(as)) do (ii, a)
         weights = (; 
             state_proximity = 1, 
             state_goal = 1,
-            control_Δvx = 1, 
-            control_Δvy = 1),
+            control_Δvx = 10, 
+            control_Δvy = 10),
         T_activate_goalcost,
         prox_min_regularization = 0.1
     )
@@ -73,7 +75,7 @@ end
 kkt_converged, kkt_solution, kkt_model = 
         solve_game(KKTGameSolver(), control_system, player_cost_models, x0, T; 
         solver = Ipopt.Optimizer, 
-        solver_attributes = (; max_wall_time = 30.0, print_level = 5))
+        solver_attributes = (;max_wall_time, print_level = 5))
 
 
 # ---- Save trajectory to file ----
