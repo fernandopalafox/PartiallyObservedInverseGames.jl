955fbcc7c70c8f363f7d6227884dc20a06603257
Fernando Palafox <fernando.palafox@colorado.edu>
2023-07-07 17:43:32 -0500

    added obs + pred visualizer

--- src/TrajectoryVisualization.jl
+++ src/TrajectoryVisualization.jl
@@ -242,7 +242,124 @@ function visualize_rotating_hyperplanes(states, params; koz = true, fps = 5)
         plot!(xlims = domain,
               ylims = domain)
     end
-    gif(anim, fps = 5, "rotating_hyperplanes_"*params.title*".gif")
+    gif(anim, fps = fps, "rotating_hyperplanes_"*params.title*".gif")
+end    
+
+function visualize_obs_pred(states, T_obs, params; koz = true, fps = 5) 
+
+    # Useful stuff
+    pos_idx = vcat(
+        [[1 2] .+ (player - 1) * params.n_states_per_player for player in 1:(params.n_players)]...,
+    )
+    couples = findall(params.adj_mat)
+    colors = palette(:default)[1:(params.n_players)]
+    T = size(states,2)
+
+    # Domain
+    x_domain = extrema(states[pos_idx[:, 1], :]) .+ (-0.01, 0.01)
+    y_domain = extrema(states[pos_idx[:, 2], :]) .+ (-0.01, 0.01)
+    domain = [minimum([x_domain[1], y_domain[1]]), maximum([x_domain[2], y_domain[2]])]    
+
+    # Define useful vectors
+    function n(t, α, ω)
+        [cos(α + ω * (t-1)), sin(α + ω * (t-1))]
+    end
+
+    function n0(couple, states, pos_idx)
+        states[pos_idx[couple[1], 1:2], 1] - states[pos_idx[couple[2], 1:2], 1] 
+    end
+
+    # Animation of trajectory 
+    anim = @animate for i = 1:T
+
+        if i <= T_obs
+            title = "$(params.title)\n t = $i/$T\nmode: observing"
+        else
+            title = "$(params.title)\n t = $i/$T\nmode: predicting"
+        end
+
+        # Plot trajectories
+        plot(; legend = false, title = title, xlabel = "x", ylabel = "y", size = (500, 500), palette = colors)
+        
+        if i <= T_obs 
+            plot!(
+                [states[pos_idx[player, 1], 1:i] for player in 1:(params.n_players)],
+                [states[pos_idx[player, 2], 1:i] for player in 1:(params.n_players)]
+            )
+            scatter!(
+                [states[pos_idx[player, 1], i] for player in 1:(params.n_players)],
+                [states[pos_idx[player, 2], i] for player in 1:(params.n_players)],
+                markersize = 5,
+                color = colors,
+            )
+        else
+            plot!(
+                [states[pos_idx[player, 1], 1:T_obs] for player in 1:(params.n_players)],
+                [states[pos_idx[player, 2], 1:T_obs] for player in 1:(params.n_players)], 
+                linestyle = :solid,
+            )
+            plot!(
+                [states[pos_idx[player, 1], T_obs:i] for player in 1:(params.n_players)],
+                [states[pos_idx[player, 2], T_obs:i] for player in 1:(params.n_players)], 
+                linestyle = :dash,
+            )
+            scatter!(
+                [states[pos_idx[player, 1], T_obs] for player in 1:(params.n_players)],
+                [states[pos_idx[player, 2], T_obs] for player in 1:(params.n_players)],
+                markersize = 5,
+                color = colors,
+            )
+            scatter!(
+                [states[pos_idx[player, 1], i] for player in 1:(params.n_players)],
+                [states[pos_idx[player, 2], i] for player in 1:(params.n_players)],
+                markersize = 5,
+                markerstrokealpha = 1.0,
+                markeralpha = 0.5,
+                color = colors,
+            )
+        end
+
+        # Plot KoZs
+        opacity = clamp((i/T_obs)^4, 0, 1)
+        for couple in couples
+            if koz
+                # Plot KoZ around hyperplane owner
+                plot!(
+                    [states[pos_idx[couple[2], 1], i] + params.ρs[couple[1], couple[2]] * cos(θ) for θ in range(0,stop=2π,length=100)], 
+                    [states[pos_idx[couple[2], 2], i] + params.ρs[couple[1], couple[2]] * sin(θ) for θ in range(0,stop=2π,length=100)], 
+                    color = colors[couple[1]], 
+                    legend = false,
+                    linealpha = 0.0,
+                    fillalpha = 0.5 * opacity,
+                    fill = true,
+                )
+            end
+            # Plot hyperplane normal
+            ni =
+                params.ρs[couple[1], couple[2]] *
+                n(i, params.αs[couple[1], couple[2]], params.ωs[couple[1], couple[2]])
+            # plot!(
+            #     [states[pos_idx[couple[2], 1], i], states[pos_idx[couple[2], 1], i] + ni[1]],
+            #     [states[pos_idx[couple[2], 2], i], states[pos_idx[couple[2], 2], i] + ni[2]],
+            #     arrow = true,
+            #     color = colors[couple[1]],
+            #     linealpha = opacity,
+            # )
+            # Plot hyperplane 
+            hyperplane_domain = 10*range(domain[1],domain[2],100)
+            p = states[pos_idx[couple[2], 1:2], i] + ni
+            plot!(hyperplane_domain .+ p[1],
+                [-ni[1]/ni[2]*x + p[2] for x in hyperplane_domain],
+                color = colors[couple[1]],
+                linealpha = 0.5 * opacity,
+            )
+        end
+
+        # Set domain
+        plot!(xlims = domain,
+              ylims = domain)
+    end
+    gif(anim, fps = fps, "obs_pred_"*params.title*".gif")
 end    
 
 function animate_trajectory(states, params)
